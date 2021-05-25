5564cda3e44194888baa541648ca29d17dd3b814
serenasensini <serena.sensini@gmail.com>
2021-05-25 17:03:57 +0200

    update capacitor

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseURL: 'http://192.168.1.66:8081/api'
+};
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: false,
+//   baseURL: 'http://localhost:8081/api'
+// };
 export const environment = {
-  production: false,
-  baseURL: 'http://localhost:8081/api'
+  production: true,
+  baseURL: 'http://192.168.1.66:8081/api'
 };
